12ab53cf285590363f32f9ed9cc62f555132eb10
Daniil <vtldtlm159@gmail.com>
2022-09-04 20:36:51 +0700

    Commit №3

--- Test FSharp project/ListData.fs
+++ Test FSharp project/ListData.fs
@@ -3,8 +3,23 @@
 open System
 //open FSharp.Interop.Excel.ExcelProvider
 open FSharp.Interop.Excel
-open Microsoft.FSharp.Collections
+open Types
 
 
 type Data = ExcelFile<"ExcelData\ListData.xlsx", ForceString=true>
-let FindByRows Index = Data |> Seq.tryFind(fun x -> x.Index=Index)
+let Data = new Data()
+let some = Data.Data |>Seq.head
+//let getDefinitionByIndex index = some |> Seq.tryFind(fun  x -> string x.Index = index)
+let Index a = a
+let Number b = b
+
+let ResIndex = Index "текст"
+let ResNumber = Number 10.5
+
+
+printfn $"Текстовый парметр Index = {ResIndex}"
+printfn $"Числовой параметр Number = {ResNumber}"
+//printfn $"Результат поиска ID = {ResFind}"
+
+printfn $"{some}"
+
